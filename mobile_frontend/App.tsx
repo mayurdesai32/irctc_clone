3ec26a9779f073bd29b363ccd9221010b4915ac2
Mayur desai <msdesai32@gmail.com>
2024-10-29 13:44:41 +0530

    file added

---
+++ mobile_frontend/App.tsx
@@ -0,0 +1,16 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+
+      <SafeAreaView style={{flex: 1, backgroundColor: 'red'}}>
+        <Text>App</Text>
+      </SafeAreaView>
+  
+  );
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ mobile_frontend/src/themes/styles.ts
@@ -0,0 +1,11 @@
+
+
+export const color ={
+  primary: '#F1F1FA',
+  secondary: '#132C78',
+  third: '#D6582B',
+  forth: 'FBEEE6',
+  fifth: 'D9D9EB',
+};
+
+
